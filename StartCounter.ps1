e219c2486aa92e9f814c69dfe2f04ba27a7f1f0f
Scr0n <46013538+Scr0n@users.noreply.github.com>
2021-06-16 15:19:28 +0700

    Create StartCounter.ps1

---
+++ StartCounter.ps1
@@ -0,0 +1,45 @@
+$servers = 
+    'askue',     #RK
+    'ASKUE-SKAL',#RS
+    'ASKUE-RM',  #RM
+    'ASKUE-MZ',  #MZ
+    'ASKUE-RT',  #RT
+    'ASKUE-RO',  #RO
+    'ASKEU-NMZ', #NMZ
+    'TOFASUE'    #TOF
+
+$i = 1
+write-host 'где запускаем counter?'
+foreach ($server in $servers)
+    {
+        write-host  "[" $i "]", $server
+        $i = $i + 1
+    }
+
+$ser = Read-Host
+if     ( $ser -eq 1 ) { $result = 'askue'     }
+elseif ( $ser -eq 2 ) { $result = 'ASKUE-SKAL'}
+elseif ( $ser -eq 3 ) { $result = 'ASKUE-RM'  }
+elseif ( $ser -eq 4 ) { $result = 'ASKUE-MZ'  }
+elseif ( $ser -eq 5 ) { $result = 'ASKUE-RT'  }
+elseif ( $ser -eq 6 ) { $result = 'ASKUE-RO'  }
+elseif ( $ser -eq 7 ) { $result = 'TOFASUE'   }
+$result
+
+write-host 'Сколько минут собирать?'
+[int]$time = Read-Host
+[int]$waitSeconds = $time * 60 
+
+Invoke-Command -ComputerName $result -ScriptBlock {logman start -n counter}
+
+Start-Sleep -Seconds 1
+$waitSeconds..0 | ForEach-Object {
+    Write-Host "Time Remaining: $_" 
+    Start-Sleep -Seconds 1}
+
+Invoke-Command -ComputerName $result -ScriptBlock {logman stop -n counter}
+
+$test = 100
+$test..0 | ForEach-Object {
+    Write-Host "Time Remaining: $_" 
+    Start-Sleep -Seconds 1}
